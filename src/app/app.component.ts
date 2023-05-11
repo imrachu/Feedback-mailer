7ca47be6fcea6487224ccd3075e35e1c9f89b0e7
Rachit Kanaujia <rachit.kanaujia@geminisolutions.com>
2023-05-11 13:07:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'feedbackMailer';
+
+  successModalRef: BsModalRef;
+  config: any = {
+    backdrop: 'static',
+    keyboard: false,
+    class: 'theme-modal',
+  };
+
+  constructor(
+    private modalService: BsModalService,
+  ) { }
+
+  openModal(
+    success_template: TemplateRef<any>
+  ) {
+    this.successModalRef = this.modalService.show(
+      success_template,
+      this.config
+    );
+  }
 }
